4e526b8836fbaa7af706773418c853856d312fa9
Felix Hermann <17felix2000@gmail.com>
2021-01-28 19:06:17 +0100

    new go

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_28_173056) do
+ActiveRecord::Schema.define(version: 2021_01_28_180448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(version: 2021_01_28_173056) do
   create_table "flats", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "offers", force: :cascade do |t|
