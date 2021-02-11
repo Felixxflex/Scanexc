2ed2e5883bda0188ee98d03e3e5ff073fc113563
Filippo Lanza <fpg.lanza@gmail.com>
2021-02-11 17:03:17 +0100

    tried working on removepoints method from winpoints controller and create a working link in sport show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  
+
   devise_for :users, controllers: { confirmations: 'confirmations' }
 
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#home"
   resources :flats
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   resources :sneakers
   resources :gadgets
   resources :businesses
- 
+
 
   get "/path/users/page", to: "users#show", as: "users"
   get "/path/technologiesnew/page", to: "technologies#new", as: "technologiesnew"
@@ -19,9 +19,9 @@ Rails.application.routes.draw do
   get "/path/questions", to: "pages#questions", as: "pagesquestions"
   get "/path/users/show", to: "users#show", as: "usersshow"
 
-  
 
-  # get "/path/users/removepoints", to: "sports#update", as: :sport_remove_points
+
+  get "/path/users/remove_points", to: "winpoints#update_points", as: :sport_remove_points
 
 
   get "/path/users/addpoints", to: "winpoints#update", as: :user_add_points
