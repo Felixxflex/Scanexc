66da41f6fa2d625cba380e0ebbe585d37a96ed6c
Cornelius Koetting <cornelius@ckdevelopment.de>
2021-03-02 14:38:01 +0100

    Basic JS Timer logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       # Deliver the signup email
-      UserNotifierMailer.send_signup_email(@user).deliver
-      redirect_to(@user, :notice => 'User created')
+      # UserNotifierMailer.send_signup_email(@user).deliver
+      # redirect_to(@user, :notice => 'User created')
     else
       render :action => 'new'
     end
